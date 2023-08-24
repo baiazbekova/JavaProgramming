13968966101c4ea2449daf7f0332de867d386566
baiazbekova <baiazbekova@gmail.com>
2023-08-24 15:26:01 -0400

    more changes

---
+++ src/practice/ArithmeticOperators.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        System.out.println(10.0/4);
+
+        System.out.println(1000+2000);
+    }
+}
---
+++ src/practice/ShortHandOperators.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class ShortHandOperators {
+    public static void main(String[] args) {
+
+        //assignment operator
+        int a = 100;
+        a=200;
+
+        //addition assignment
+        int b = 200;
+        b+=100;
+
+        //subtraction
+        int c = 300;
+        c-=100;
+
+
+
+    }
+}
