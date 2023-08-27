501560d8809906467f5854bca939609c002487f9
baiazbekova <baiazbekova@gmail.com>
2023-08-27 11:56:19 -0400

    methods

---
+++ src/practice/CustomMethod_Void.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class CustomMethod_Void {
+
+    public static void main(String[] args) {
+        displayMessage();
+    }
+
+    public static void displayMessage(){
+        System.out.println("Hello World");
+        System.out.println("I love Java");
+    }
+
+}
---
+++ src/practice/CustomeMethod_Parameters.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class CustomeMethod_Parameters {
+    public static void main(String[] args) {
+        displayValue(7);
+    }
+
+    public static void displayValue (int num){
+
+        System.out.println("The value is: "+ num); //7
+    }
+}
