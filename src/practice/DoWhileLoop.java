ab893461ef4da82c4ddcf813753029a52cbdd368
baiazbekova <baiazbekova@gmail.com>
2023-08-31 07:05:21 -0400

    loops and overloading

---
+++ src/practice/DoWhileLoop.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+
+      int  i = 5;
+//        do {
+//            System.out.println("Hello"); //Hello ( it will print at least 1 time)
+//        }
+//        while (i<7);
+
+        System.out.println("----------------------");
+
+//        while (i>=5){
+//            System.out.println("Hello"); //will not be executed
+//        }
+//
+//        System.out.println("----------------");
+
+        do{
+            System.out.println("Hello");
+            i++;
+        }while (i<=6);
+
+
+    }
+}
---
+++ src/practice/NestedLoop.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class NestedLoop {
+    public static void main(String[] args) {
+        //print 1 ~ 5 5 times
+        for (int j = 1; j <= 5; j++) {
+
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(i + "");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
