26458a228de669c34d0c57c4f7192632b1528bdf
baiazbekova <baiazbekova@gmail.com>
2023-09-20 16:14:13 -0400

    abstractclasses

--- src/abstractMethods/ShapeObjects.java
+++ src/abstractMethods/ShapeObjects.java
@@ -5,7 +5,7 @@ public class ShapeObjects {
     public static void main(String[] args) {
 
 
-        Shape shape = new Shape(); //its impossible to create an object from an abstract class
+        //Shape shape = new Shape(); //its impossible to create an object from an abstract class
         Circle circle = new Circle();
         Rectangle rectangle = new Rectangle();
         Square square = new Square();
